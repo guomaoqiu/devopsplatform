8233bd4e7e03b874b616eb31868e67e5f96e221d
guomaoqiu <guomaoqiu@gmail.com>
2018-03-01 10:19:56 +0800

    frist commit

--- app/email.py
+++ app/email.py
@@ -17,6 +17,6 @@ def send_email(to, subject, template, **kwargs):
                   sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])
     msg.body = render_template(template + '.txt', **kwargs)
     msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
+    thr = Thread(target=send_async_email, args=[app, msg]) 
     thr.start()
     return thr
