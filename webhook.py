d8a39e0439337f327c2fdf81cbcb8a1f00794b4b
guomaoqiu@sina.com <guomaoqiu@sina.com>
2018-03-23 17:15:57 +0800

    frist commit

--- webhook.py
+++ webhook.py
@@ -12,6 +12,7 @@ git_url = "git@github.com:guomaoqiu/devopsplatform.git"
 app = Flask(__name__)
   # 
 #重启服务
+#sss
 restart_services = os.system("systemctl restart supervisord")
 
 @app.route('/pullcode', methods=['POST'])
